915376d0ca12211ce628a4e507530efd6fbca735
Andrew Wu <arw@dmp.com.tw>
2022-09-28 16:25:59 +0800

    Make link : ~/.vimrc -> .vim/vimrc

--- init-vim.sh
+++ init-vim.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 DOTVIMDIR=`dirname $0`
+F=vimrc
+S=~/.$F
+T=$DOTVIMDIR/$F
+
+if stat $S > /dev/null 2>/dev/null; then
+  # ~/.FILE already exist
+  if [[ `readlink $S` == $T ]]; then
+    echo "$F : Linked to right place."
+  else
+    echo "$F : Already exist, need fix?"
+  fi
+else
+  # ~/.FILE not exist, create link
+  if ln -s $T $S; then
+    echo "$F : Create link to $T"
+  else
+    echo "$F : Create link failed!"
+  fi
+fi
+
 (cd $DOTVIMDIR && git submodule init && git submodule update)
 vim -c "execute \"PluginInstall\" | qa"
